7f575c074f23dc8e6c4f5d50bec96a70d10c9131
shamsherthind <82254061+shamsherthind@users.noreply.github.com>
2024-09-19 14:02:38 -0400

    updated data file names

--- src/datasets/segment3d.py
+++ src/datasets/segment3d.py
@@ -216,10 +216,15 @@ class SEGMENT3D(BaseDataset):
             `{'train': [...], 'val': [...], 'test': [...]}`
         """
         return    {
-            'train': ['DrumPlant_Tank_rotate45_classified8.las'],
-            'val': ['DrumPlant_Tank_rotate45_classified_sample1.las', 'DrumPlant_Tank_rotate45_classified_sample2.las'],
-            'test':  ['DrumPlant_Tank_rotate45_classified4.las'] 
+            'train': ['DrumPlant_Tank_rotate45_classified_sample1.las'],
+            'val': ['DrumPlant_Tank_rotate45_classified_sample2.las'],
+            'test':  ['DrumPlant_Tank_rotate45_classified_small.las'] 
             }
+        # return    {
+        #     'train': ['DrumPlant_Tank_rotate45_classified8.las'],
+        #     'val': ['DrumPlant_Tank_rotate45_classified_sample1.las', 'DrumPlant_Tank_rotate45_classified_sample2.las'],
+        #     'test':  ['DrumPlant_Tank_rotate45_classified4.las'] 
+        #     }
     
     # {
     #         'train': [f'Area_{i}' for i in range(1, 7) if i != self.fold],
